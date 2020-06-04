6fcf9ecf51e031a5988dfc09ba21205f4125ddda
wpf <353485896@qq.com>
2020-06-04 10:36:58 +0800

    搭建项目，配置环境，第一次提交

---
+++ vue-admin-copy/src/views/login/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="login">
+        这是登陆页面
+        <el-button type="success">成功按钮</el-button>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+    #login{
+        height: 100vh;
+        background-color: cornflowerblue;
+    }
+</style>
